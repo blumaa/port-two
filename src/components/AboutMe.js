d86d9420b2f63d53de75b8da70344cd94a4b2cfe
blumaa <blumaa@gmail.com>
2020-04-29 21:09:14 +0200

    major updates to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,37 +1,53 @@
 import React from "react";
 import ScrollAnimation from "react-animate-on-scroll";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import downarrow from "../images/downarrow.svg";
+import { Link } from "react-scroll";
 
 const AboutMe = () => {
   return (
     <>
       <div id="about-me">
         <div className="grid-container">
-          <div className="long-row-top">
-
-              <div id="title-box">
-                <div id="title">Aaron Blum</div>
-                {/* <div id="sub-title">front end developer</div> */}
-              </div>
-          </div>
-          <div id="about-me-main-container">
-            <div id="picture-box">
-
-            </div>
+          <div className="about-me-top-container">
             <div id="title-box">
-            <h1 >Example</h1>
-
+              <div id="title">Aaron Blum</div>
+              {/* <div id="sub-title">front end developer</div> */}
             </div>
-            <div id="description-box">
-
-            <p>
-            Full stack web developer with a passion for teaching. I thrive within an innovative team, one that will challenge me and where I can add my creativity, humor, and coding expertise. My passion for playing music allows me to think outside the box and engineer web applications from a creative, audience oriented perspective. I am excited to connect people through technology in an engaging way, like I helped bridge people through education.
-
-            </p>
+          </div>
+          <div id="about-me-middle-container">
+            {/* <div id="picture-box"></div> */}
+            <div id="title-box" className="animated fadeInRight">
+              Hi, my name is Aaron. I'm a front-end developer.
             </div>
-
+            <div id="main-box-one"  className="animated bounceInLeft slow">
+                I bring organization and creativity to every project, as well as
+                a relentless drive to realize each clients' dream.
             </div>
+          </div>
           {/* <div className="long-row-middle-2">long-row-middle</div> */}
-          <div className="about-me-bottom-container">long-row-bottom</div>
+          <div className="about-me-bottom-container">
+            <div id="bottom-one"></div>
+            <div id="bottom-two"></div>
+            <div id="bottom-three" className="animated bounceInLeft slower">
+              <div id="down-button">
+                <Link
+                  to="page2"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={250}
+                >
+                  <img
+                    src={downarrow}
+                    id="accordion-icon"
+                    className="animated wobble slower delay-2s"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
